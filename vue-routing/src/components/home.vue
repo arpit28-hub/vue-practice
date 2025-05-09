216303aca4cdcf9d25f7e66c503f609d38a430b8
Arpit Kumar <arpitkumar592001@gmail.com>
2025-05-09 17:07:26 +0530

    vuetify.js setup

---
+++ vue-routing/src/components/home.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <v-btn></v-btn>
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
---
+++ vue-routing/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+<template>
+  <v-row class="align-center" style="border: 1px solid #ccc; padding: 8px;">
+    <v-btn color="primary">Left</v-btn>
+
+    <!-- This div takes up all the remaining space -->
+    <div class="flex-fill" style="background: #eee; height: ;">hello</div>
+
+    <v-btn color="secondary">Right</v-btn>
+  </v-row>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
